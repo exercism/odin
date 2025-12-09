60affaab113fda113da3a459dbf05a5c05c78d14
Robert Monnet <rcmonnet@gmail.com>
2025-12-09 08:46:10 -0500

    Exercise Perfect Numbers from PR #37 (author gvrooyen) (#137)
    The 11 exercises in PR #37 have been stuck in limbo since last
    year.
    
    They were mostly complete except for the description tags and
    some minor changes to match the track convention.
    
    This PR just includes the update work.

--- dev/tools/tagfixer/to_snake_case.sh
+++ dev/tools/tagfixer/to_snake_case.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-to_snake_case() {
-    sed -E '
-        s/[ -]/_/g
-        s/([[:lower:][:digit:]])([[:upper:]])/\1_\2/g
-        s/ = /_equals_/g
-        s/[^[:alnum:]_]//g
-    ' | tr '[:upper:]' '[:lower:]'
-}
-
 if [[ $# -ne 1 ]]; then
   echo "Usage: to_snake_case <sentence>"
   exit 1
 fi
 
-to_snake_case "$1"
+echo "$1" | sed -E '
+        s/[ -]/_/g
+        s/([[:lower:][:digit:]])([[:upper:]])/\1_\2/g
+        s/ = /_equals_/g
+        s/[^[:alnum:]_]//g
+    ' | tr '[:upper:]' '[:lower:]'
---
+++ exercises/practice/perfect-numbers/perfect_numbers.odin
@@ -0,0 +1,13 @@
+package perfect_numbers
+
+Classification :: enum {
+	Perfect,
+	Abundant,
+	Deficient,
+	Undefined,
+}
+
+classify :: proc(number: uint) -> Classification {
+	// Implement the procedure.
+	return .Undefined
+}
